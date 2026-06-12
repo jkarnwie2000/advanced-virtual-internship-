85d8f8b2a81193e8db82f9328bf31277738d857c
jkarnwie2000 <jkarnwie2000@yahoo.com>
2026-06-12 02:20:39 -0400

    test run

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,22 @@ import AuthModal from "./AuthModal";
 import { useState } from "react";
 import Homesections from "./Homesections";
 import Footer from "./Footer";
+import Navbarsection from "./Navbarsection";
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <>
+      <Navbarsection />
       <Hero setIsModalOpen={setIsModalOpen} />
-      {isModalOpen && (
-        <>
       <Homesections />
       <Footer />
+      {isModalOpen && (
+        <>
+      
           <AuthModal onClose={() => setIsModalOpen(false)} />
           <div className="dark-overlay" />
+          
         </>
       )}
     </>
