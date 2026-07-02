7326cc55205c1f3a3f19c37ef127926b2b51f635
jkarnwie2000 <jkarnwie2000@yahoo.com>
2026-07-02 04:41:19 -0400

    test run

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -215,9 +215,9 @@ export default function BookPage() {
 <div className="inner-book__tag">Communication Skills</div>
 <div className="inner-book__tag">Technology &amp; the Future</div>
 </div>
-<div className="inner-book__book--description">"How to Win Friends and Influence People" is a self-help book written by Dale Carnegie and first published in 1936. The book provides practical advice and techniques for improving one's communication and social skills, with the goal of building better relationships and becoming more influential in both personal and professional settings. The book covers topics such as the importance of smiling, listening actively, giving honest and sincere appreciation, avoiding criticism, and becoming genuinely interested in others. It also emphasizes the power of empathy and understanding other people's perspectives. "How to Win Friends and Influence People" has been widely read and praised for its timeless and practical advice, and is considered a classic in the field of self-improvement.</div>
+<div className="inner-book__book--description">{book?.bookDescription}</div>
 <h2 className="inner-book__secondary--title">About the author</h2>
-<div className="inner-book__author--description">Dale Carnegie (1888-1955) was an American author, lecturer, and developer of self-improvement courses. He is best known for his book "How to Win Friends and Influence People," which has sold over 30 million copies worldwide and is considered a classic in the self-help genre. Carnegie's teachings focused on improving interpersonal skills, communication, and leadership, and his courses and books were aimed at helping individuals become more confident, successful, and influential in both their personal and professional lives. He also founded the Dale Carnegie Training program, which is still in operation today and has helped millions of people around the world improve their communication and leadership skills.</div>
+<div className="inner-book__author--description">{book?.authorDescription}</div>
 </div>
 <div className="inner-book--img-wrapper">
 <figure className="book__image--wrapper" style={{"height": "300px", "width": "300px", "minWidth": "300px"}}>
