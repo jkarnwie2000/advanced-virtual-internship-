ebf6d6b954547c6d11cf71315e180af79c372cab
jkarnwie2000 <jkarnwie2000@yahoo.com>
2026-07-05 11:54:04 -0400

    test run

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useParams } from "next/navigation";
-import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
+import { auth } from "../../../firebaseConfig";
+import AuthModal from "../../AuthModal";
+
 
 export default function BookPage() {
   const params = useParams();
   const bookId = params.id;
+
+  const router = useRouter();
+  const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
 
   const [book, setBook] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -36,9 +41,38 @@ export default function BookPage() {
   }
 
 
+
+
+
+
+
+
+  const handleReadListen = () => {
+  const user = auth.currentUser;
+
+  if (!user) {
+    setIsAuthModalOpen(true);
+    return;
+  }
+
+  if (book.subscriptionRequired) {
+    router.push("/choose-plan");
+    return;
+  }
+
+  router.push(`/player/${bookId}`);
+};
+
+
+
  return ( 
 
 <div id="__next">
+
+ {isAuthModalOpen && (
+      <AuthModal onClose={() => setIsAuthModalOpen(false)} />
+    )}
+
 <div className="wrapper">
 <div className="search__background">
 <div className="search__wrapper">
@@ -187,22 +221,22 @@ export default function BookPage() {
 </div>
 </div>
 <div className="inner-book__read--btn-wrapper">
-<Link href={`/player/${bookId}`} className="inner-book__read--btn">
+<button onClick={handleReadListen} className="inner-book__read--btn">
 <div className="inner-book__read--icon">
 <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
 <path d="M928 161H699.2c-49.1 0-97.1 14.1-138.4 40.7L512 233l-48.8-31.3A255.2 255.2 0 0 0 324.8 161H96c-17.7 0-32 14.3-32 32v568c0 17.7 14.3 32 32 32h228.8c49.1 0 97.1 14.1 138.4 40.7l44.4 28.6c1.3.8 2.8 1.3 4.3 1.3s3-.4 4.3-1.3l44.4-28.6C602 807.1 650.1 793 699.2 793H928c17.7 0 32-14.3 32-32V193c0-17.7-14.3-32-32-32zM324.8 721H136V233h188.8c35.4 0 69.8 10.1 99.5 29.2l48.8 31.3 6.9 4.5v462c-47.6-25.6-100.8-39-155.2-39zm563.2 0H699.2c-54.4 0-107.6 13.4-155.2 39V298l6.9-4.5 48.8-31.3c29.7-19.1 64.1-29.2 99.5-29.2H888v488zM396.9 361H211.1c-3.9 0-7.1 3.4-7.1 7.5v45c0 4.1 3.2 7.5 7.1 7.5h185.7c3.9 0 7.1-3.4 7.1-7.5v-45c.1-4.1-3.1-7.5-7-7.5zm223.1 7.5v45c0 4.1 3.2 7.5 7.1 7.5h185.7c3.9 0 7.1-3.4 7.1-7.5v-45c0-4.1-3.2-7.5-7.1-7.5H627.1c-3.9 0-7.1 3.4-7.1 7.5zM396.9 501H211.1c-3.9 0-7.1 3.4-7.1 7.5v45c0 4.1 3.2 7.5 7.1 7.5h185.7c3.9 0 7.1-3.4 7.1-7.5v-45c.1-4.1-3.1-7.5-7-7.5zm416 0H627.1c-3.9 0-7.1 3.4-7.1 7.5v45c0 4.1 3.2 7.5 7.1 7.5h185.7c3.9 0 7.1-3.4 7.1-7.5v-45c.1-4.1-3.1-7.5-7-7.5z"></path>
 </svg>
 </div>
 <div className="inner-book__read--text">Read</div>
-</Link>
-<Link href={`/player/${bookId}`} className="inner-book__read--btn">
+</button>
+<button onClick={handleReadListen} className="inner-book__read--btn">
 <div className="inner-book__read--icon">
 <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
 <path d="M842 454c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8 0 140.3-113.7 254-254 254S258 594.3 258 454c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8 0 168.7 126.6 307.9 290 327.6V884H326.7c-13.7 0-24.7 14.3-24.7 32v36c0 4.4 2.8 8 6.2 8h407.6c3.4 0 6.2-3.6 6.2-8v-36c0-17.7-11-32-24.7-32H548V782.1c165.3-18 294-158 294-328.1zM512 624c93.9 0 170-75.2 170-168V232c0-92.8-76.1-168-170-168s-170 75.2-170 168v224c0 92.8 76.1 168 170 168zm-94-392c0-50.6 41.9-92 94-92s94 41.4 94 92v224c0 50.6-41.9 92-94 92s-94-41.4-94-92V232z"></path>
 </svg>
 </div>
 <div className="inner-book__read--text">Listen</div>
-</Link>
+</button>
 </div>
 <div className="inner-book__bookmark">
 <div className="inner-book__bookmark--icon">
