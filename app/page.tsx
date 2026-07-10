c1864c42e9c25890d512978aa0aa8bc08b0e12d2
jkarnwie2000 <jkarnwie2000@yahoo.com>
2026-07-09 21:13:53 -0400

    test run

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import Hero from "./Hero";
+import AuthModal from "./AuthModal";
+import { useState } from "react";
+import Homesections from "./Homesections";
+import Footer from "./Footer";
+import Navbarsection from "./Navbarsection";
 
 export default function Home() {
-  redirect("/foryou");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <>
+      <Navbarsection />
+      <Hero setIsModalOpen={setIsModalOpen} />
+      <Homesections />
+      <Footer />
+      {isModalOpen && (
+        <>
+      
+          <AuthModal onClose={() => setIsModalOpen(false)} />
+          <div className="dark-overlay" />
+          
+        </>
+      )}
+    </>
+  );
 }
